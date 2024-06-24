1d56d67cfd418e4d49ce58712853117cd23add5b
Devan Tera Bayu <devan.terabayu81@gmail.com>
2024-06-25 02:03:22 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,12 +2,12 @@ import "@styles/globals.css";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
-
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
